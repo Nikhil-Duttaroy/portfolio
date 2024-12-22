62dd52d85eeb591f673c57642574fa3293076885
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-22 21:49:24 +0530

    Implemented component to embed calendly to website

---
+++ src/components/CalendlyEmbed.component.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+
+interface CalendlyEmbedProps {
+  url: string;
+  text: string;
+}
+
+const CalendlyEmbed = ({ url, text }: CalendlyEmbedProps) => {
+  useEffect(() => {
+    const head = document.querySelector("head");
+    const link = document.createElement("link");
+    link.setAttribute(
+      "href",
+      "https://assets.calendly.com/assets/external/widget.css"
+    );
+    link.setAttribute("rel", "stylesheet");
+    head?.appendChild(link);
+
+    const script = document.createElement("script");
+    script.setAttribute(
+      "src",
+      "https://assets.calendly.com/assets/external/widget.js"
+    );
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("async", "true");
+    head?.appendChild(script);
+  }, []);
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    window.Calendly.initPopupWidget({ url });
+  };
+
+  return (
+    <a href="#" onClick={handleClick}>
+      {text}
+    </a>
+  );
+};
+
+export default CalendlyEmbed;
