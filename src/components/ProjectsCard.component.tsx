35852c367034992ad0302a6bb8fd2ec413ed9763
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-25 20:37:31 +0530

    Enhanced Product Card mobile responsivness

--- src/components/ProjectsCard.component.tsx
+++ src/components/ProjectsCard.component.tsx
@@ -13,24 +13,26 @@ const ProjectsCard = () => {
         <FaBriefcase className="text-primaryAccent" size={24} />
         Projects
       </h1>
-      <div className="grid grid-cols-1 gap-4 transition-all cursor-default overflow-y-auto">
+      <div className="grid grid-cols-1 gap-4 transition-all cursor-default overflow-y-auto overflow-x-hidden">
         {projectDetails.map((project) => (
           <a
             key={project.name}
             href={project.siteLink}
             target="_blank"
-            className=" border border-primaryBorder text-primaryForeground px-8 py-2 rounded-3xl flex justify-start items-center gap-4 group"
+            className="border border-primaryBorder text-primaryForeground px-8 py-2 rounded-3xl flex flex-col md:flex-row justify-start items-center gap-4 group"
           >
             <Image
               src={project.imgLink}
               alt={project.name}
               width={100}
               height={100}
-              className="object-cover rounded-3xl aspect-square "
+              className="object-cover rounded-3xl aspect-[16/10] md:aspect-square  w-full md:w-auto"
               priority
             />
-            <div className="flex flex-col gap-2">
-              <span className="text-primaryForeground">{project.name}</span>
+            <div className="flex flex-col gap-2 items-center md:items-start">
+              <span className="text-primaryForeground text-base md:text-xl font-semibold">
+                {project.name}
+              </span>
               <button
                 className="bg-secondaryBackground px-4 py-1 rounded-3xl flex items-center gap-2 w-fit"
                 onClick={() => window.open(project.githubLink, "_blank")}
