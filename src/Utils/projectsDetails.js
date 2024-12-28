9613966dcdbd073e7d383a6b253f518a7d57a827
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-28 11:16:18 +0530

    Updated Project card Ui to accomodate description

--- src/Utils/projectsDetails.js
+++ src/Utils/projectsDetails.js
@@ -2,6 +2,8 @@ export const projectDetails = [
   {
     id: 1,
     name: "TextCollabo",
+    description:
+      "A real-time text editor that allows multiple users to collaborate on a room simultaneously.It uses React,Socket.io and Codemirror for real-time collaboration.",
     imgLink: "/assets/TextCollabo.png",
     siteLink: "https://textcollabo.onrender.com/",
     githubLink: "https://github.com/Nikhil-Duttaroy/Online-Text-Collab",
@@ -9,6 +11,8 @@ export const projectDetails = [
   {
     id: 2,
     name: "Shoppers Loop",
+    description:
+      "A Voice Enabled E-commerce website. It uses React, Firebase, and Alan AI for voice recognition.",
     imgLink: "/assets/Shoppers.png",
     siteLink: "https://shoppersloop.onrender.com/",
     githubLink: "https://github.com/Nikhil-Duttaroy/ShoopersLoop",
@@ -16,6 +20,8 @@ export const projectDetails = [
   {
     id: 3,
     name: "Youtube Clone",
+    description:
+      "A Front-End Youtube clone. It uses the Youtube API to do all that Youtube does.",
     imgLink: "/assets/Youtube.png",
     siteLink: "https://reactytclone.web.app/login",
     githubLink: "https://github.com/Nikhil-Duttaroy/Youtube-Clone",
--- src/components/ProjectsCard.component.tsx
+++ src/components/ProjectsCard.component.tsx
@@ -4,9 +4,11 @@ import Card from "./Card/Card.component";
 import { FaBriefcase } from "react-icons/fa";
 import { projectDetails } from "../Utils/projectsDetails";
 import Image from "next/image";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGlobe } from "react-icons/fa";
+
 import CardTitle from "./Card/CardTitle.component";
 import CardList from "./Card/CardList.component";
+import Pill from "./Pill.component";
 
 const ProjectsCard = () => {
   return (
@@ -14,36 +16,45 @@ const ProjectsCard = () => {
       <CardTitle title="Projects" icon={FaBriefcase} />
       <CardList
         items={projectDetails}
-        className="grid grid-cols-1 lg:grid-cols-2 gap-4 overflow-y-auto overflow-x-hidden w-full"
+        className="grid grid-cols-1 lg:grid-cols-1 gap-4 overflow-y-auto overflow-x-hidden w-full"
         renderItem={(project) => (
-          <a
+          <div
             key={project.name}
-            href={project.siteLink}
-            target="_blank"
-            className="border border-primaryBorder text-primaryForeground px-2 py-2 rounded-3xl flex flex-col justify-start items-center gap-4 group "
+            className="border border-primaryBorder text-primaryForeground px-2 py-2 rounded-3xl flex flex-col lg:flex-row justify-start items-start gap-4"
           >
             <Image
               src={project.imgLink}
               alt={project.name}
               width={100}
               height={100}
-              className="object-cover rounded-3xl aspect-[16/9]"
+              className="object-cover rounded-3xl aspect-viedo self-center w-full lg:w-auto"
               style={{ width: "auto", height: "auto" }}
               priority
             />
-            <div className="flex flex-col gap-2 items-center">
-              <span className="text-primaryForeground text-base lg:text-xl font-semibold">
+            <div className="flex flex-col gap-2 items-center justify-around h-full w-full">
+              <div className="text-primaryForeground text-base lg:text-xl font-semibold flex flex-col items-center lg:items-baseline">
                 {project.name}
-              </span>
-              <button
-                className="bg-secondaryBackground px-4 py-1 rounded-3xl flex items-center gap-2 w-fit"
-                onClick={() => window.open(project.githubLink, "_blank")}
-              >
-                <FaGithub size={16} />
-                Github
-              </button>
+                <span className="text-secondaryForeground text-xs font-normal leading-3">
+                  {project.description}
+                </span>
+              </div>
+
+              <div className="flex flex-col lg:flex-row items-center gap-2">
+                <Pill
+                  name="Live Project"
+                  url={project.siteLink}
+                  icon={<FaGlobe />}
+                  className="px-4 py-1 w-fit justify-center items-center gap-4 hover:text-primaryAccent hover:outline hover:outline-primaryAccent"
+                />
+                <Pill
+                  name="Github"
+                  url={project.githubLink}
+                  icon={<FaGithub />}
+                  className="px-4 py-1 w-fit justify-center items-center gap-4 hover:text-primaryAccent hover:outline hover:outline-primaryAccent"
+                />
+              </div>
             </div>
-          </a>
+          </div>
         )}
       />
     </Card>
