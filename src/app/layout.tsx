36ea425863ce5b57aed053179359a9cf7a12ce2a
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-30 20:20:42 +0530

    Added better quality image and added basic meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+
       <body
         className={`${Fira.className} ${geistSans.className} ${geistMono.className} antialiased`}
       >
