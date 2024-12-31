ea64b2f83a69110fa6e593d99ffb114a8216c923
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-31 15:46:41 +0530

    Added  extra padding for extralarge screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       initial={{ scale: 0, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
-      className=" w-full h-dvh p-12 flex flex-col lg:grid lg:grid-cols-12 lg:grid-rows-9 gap-4 bg-darkBackground"
+      className=" w-full h-dvh p-12 2xl:p-24 flex flex-col lg:grid lg:grid-cols-12 lg:grid-rows-9 gap-4 bg-darkBackground"
     >
       <IntroCard />
       <ProjectCounter />
