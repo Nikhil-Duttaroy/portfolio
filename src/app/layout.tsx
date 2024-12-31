38b47f80e32392a947bc73c46dd1deb8386b03db
Nikhil-Duttaroy <nsdr2000@gmail.com>
2024-12-31 08:27:00 +0530

    Added Metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Fira_Code, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,6 +19,45 @@ const Fira = Fira_Code({
 export const metadata: Metadata = {
   title: "Nikhil Duttaroy",
   description: "Nikhil Duttaroy's Portfolio website as a Software Developer",
+  keywords: [
+    "Nikhil Duttaroy",
+    "Nikhil",
+    "Duttaroy",
+    "Software Developer",
+    "Front End Developer",
+    "Portfolio",
+    "Website",
+    "Personal Website",
+    "React",
+    "Next.js",
+    "Tailwind CSS",
+    "TypeScript",
+    "JavaScript",
+  ],
+  openGraph: {
+    title: "Nikhil Duttaroy",
+    description: "Nikhil Duttaroy's Portfolio website",
+    type: "website",
+    url: "https://www.nikhilduttaroy.com",
+    siteName: "Nikhil Duttaroy",
+    //get a hosted image and add it here
+    // images: [
+    //   {
+    //     url: "dummy image",
+    //     width: 800,
+    //     height: 600,
+    //     alt: "Nikhil Duttaroy",
+    //   },
+    // ],
+    locale: "en_IN",
+  },
+};
+
+const Viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -28,11 +67,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-
       <body
         className={`${Fira.className} ${geistSans.className} ${geistMono.className} antialiased`}
       >
